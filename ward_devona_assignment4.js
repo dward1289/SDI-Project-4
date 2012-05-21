43cb15433a159f278ffe4bafe533a44622aef18d
devona <dward1289@fullsail.edu>
2012-05-21 17:03:37 -0400

    String number returned as actual number value

--- ward_devona_assignment4.js
+++ ward_devona_assignment4.js
@@ -1,5 +1,19 @@
 
 //Return a number string as a number of is acutal value.
 //Insert any number as a string within the parseFloat(), and that number in the string will have its actual value.
-console.log(parseFloat());
+var realNum = function (i) {
+console.log(parseInt(i));
+}
+
+//Formats a number to use 2 decimal places.
+//Insert any number in the decimal().
+var decimal = function (n) {
+	var num = n;
+	num.toFixed(2);
+	console.log(num.toFixed(2));
+	};
+
+
+
+
 
