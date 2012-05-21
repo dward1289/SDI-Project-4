26afca624a1976f11f2470e85492a34ccc837646
devona <dward1289@fullsail.edu>
2012-05-21 16:18:40 -0400

    Convert number string to have an actual number value has been added.

--- ward_devona_assignment4.js
+++ ward_devona_assignment4.js
@@ -1,1 +1,5 @@
-alert("JavaScript works!");
+
+//Return a number string as a number of is acutal value.
+//Insert any number as a string within the parseFloat(), and that number in the string will have its actual value.
+console.log(parseFloat());
+
